35fdc0b0df51059f538ad4510e3260290c5fedee
GoodnewsPerfect <samuelgoodnews15@gmail.com>
2021-05-04 13:11:05 +0100

    Comment added to my Djangoblog

--- djangoblog/models.py
+++ djangoblog/models.py
@@ -5,11 +5,14 @@ from django.urls import reverse
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    comment = models.CharField(max_length=140)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE, )
     body = models.TextField()
 
     def __str__(self):
         return self.title
+        return self.comment
 
     def get_absolute_url(self):  # new
         return reverse('post_detail', args=[str(self.id)])
+        return reverse('home')
